64f1df1888af224c1c92939401fdc615c12738cd
Sahrane_Guassemi <sahranedev@gmail.com>
2026-01-03 15:48:28 +0100

    fix de la plage ipv6 pour le container docker en production

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap().catch((err) => {
